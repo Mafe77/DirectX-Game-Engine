afd99929a6537c1e7117be09edc80052161fbd75
Mafe77 <fefepanda@gmail.com>
2026-04-02 16:36:46 -0400

    Logger macros

--- DirectXGame/DX3D/Source/DX3D/Graphics/RenderSystem.cpp
+++ DirectXGame/DX3D/Source/DX3D/Graphics/RenderSystem.cpp
@@ -1,4 +1,5 @@
 #include <DX3D/Graphics/RenderSystem.h>
+#include <DX3D/Graphics/GraphicsLogUtils.h>
 
 
 dx3d::RenderSystem::RenderSystem(const RenderSystemDesc& desc) : Base(desc.base)
@@ -10,14 +11,11 @@ dx3d::RenderSystem::RenderSystem(const RenderSystemDesc& desc) : Base(desc.base)
 	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 
-	auto hr = D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, NULL, 0,
-		D3D11_SDK_VERSION, &m_d3dDevice, &featureLevel, &m_d3dContext);
+	 DX3DGraphicsLogErrorAndThrow(D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, 
+		NULL, createDeviceFlags, NULL, 0, D3D11_SDK_VERSION, 
+		&m_d3dDevice, &featureLevel, &m_d3dContext),
+		"Direct3D11 initialization failed.");
 
-	if (FAILED(hr))
-	{
-		getLogger().log(Logger::LogLevel::Error, "Direct3D11 initialization failed.");
-		throw std::runtime_error("Direct3D11 initialization failed.");
-	}
 }
 
 dx3d::RenderSystem::~RenderSystem()
